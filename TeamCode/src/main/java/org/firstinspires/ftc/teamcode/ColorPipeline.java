089fbff91ab21b054439462792279b1ba6e4d4ee
WillRages <90067249+WillRages@users.noreply.github.com>
2022-11-17 17:00:48 -0600

    Extract ColorPipeline
    -remove the color pipeline interface and replace it with a class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ColorPipeline.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ColorPipeline.java
@@ -8,33 +8,65 @@ import org.opencv.core.Size;
 import org.opencv.imgproc.Imgproc;
 import org.openftc.easyopencv.OpenCvPipeline;
 
-public interface ColorPipeline {
-    public class ConvertToGreyPipeline extends OpenCvPipeline
-    {
-        // Notice this is declared as an instance variable (and re-used), not a local variable
-        Mat grey = new Mat();
 
-        Telemetry telemetry;
-        public ConvertToGreyPipeline(Telemetry t) {telemetry = t;}
-        @Override
-        public Mat processFrame(@NonNull Mat input) {
-            return input;
+public class ColorPipeline extends OpenCvPipeline {
+    Mat inputMask = new Mat();
+    Size boxSize = new Size(5, 5);
+    Mat mask = new Mat(new Size(1282,722), org.opencv.core.CvType.CV_8U);
+    org.opencv.core.Point seed = new org.opencv.core.Point(640, 360);
+    Scalar one = new Scalar(1);
+
+    private static char checkForColor(double r, double g, double b, double error) {
+        if (255-error < r+g+b || r+g+b < 255+error) {
+            char[] rgb = {'r', 'g', 'b'};
+            double[] findMax = {r, g, b};
+
+            int currentMaxIndex = 0;
+            for (int i=0; i < 3; i++) {
+                if (findMax[i] > findMax[currentMaxIndex]) {
+                    currentMaxIndex = i;
+                }
+            }
+
+            return rgb[currentMaxIndex];
+        }
+        return 'e';
     }
-//            Mat inputMask = new Mat();
-//            input.copyTo(inputMask);
-//            Imgproc.Canny(inputMask, inputMask, 250, 800);
-//            Imgproc.blur(inputMask, inputMask, new Size(5, 5));
-//            telemetry.addData("Image size: ", ""+input.size());
-//            telemetry.update();
+
+
+    @Override
+    public Mat processFrame(@NonNull Mat input) {
+        //screen is 720 by 1280
+        input.copyTo(inputMask);
+//            Imgproc.Canny(input, inputMask, 250, 800);
+//            Imgproc.blur(inputMask, inputMask, boxSize);
 //            //assuming Mat.size() gets mat size
-//            Size imgDims = input.size();
-//            Mat mask = new Mat();
-//            mask.reshape(((int) imgDims.width), ((int) imgDims.height));
-//            org.opencv.core.Point seed = new org.opencv.core.Point(imgDims.width/2, imgDims.height/2);
-//            Imgproc.floodFill(inputMask, mask, seed, new Scalar(1));
+//            Imgproc.floodFill(inputMask, mask, seed, one);
 //            Imgproc.threshold(inputMask,inputMask,254,255,Imgproc.THRESH_BINARY);
-//            Mat masked = input.mul(inputMask);
-//            return masked;
-      //  }
+//
+//            telemetry.addData("Image size: ", ""+input.size().toString());
+//            telemetry.addData("Mask size: ", ""+inputMask.size().toString());
+//            telemetry.update();
+
+//            input.mul(inputMask);
+
+        return input;
+    }
+
+    public char getColor() {
+        double[] rgb = inputMask.get(360, 640);
+        if (rgb != null) {
+            if (rgb.length == 4) {
+                double r = rgb[0];
+                double g = rgb[1];
+                double b = rgb[2];
+                return checkForColor(r, g, b, 70);
+            }
+        }
+        return 'a';
+    }
+
+    public double[] getMiddlePixel() {
+        return inputMask.get(640, 360);
     }
 }
