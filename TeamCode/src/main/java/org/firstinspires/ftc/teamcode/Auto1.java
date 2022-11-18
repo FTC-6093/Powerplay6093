66addd4ce0b2b21823b465965b0147853715043d
FTC-6093 <6093deviation@gmail.com>
2022-11-17 19:46:48 -0600

    Extract colorpipeline
    Merge in changes from will-colorspace
    use HSV for color detection

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto1.java
@@ -58,52 +58,6 @@ public class Auto1 extends LinearOpMode {
         return 'e';
     }
 
-    public class ColorPipeline extends OpenCvPipeline implements org.firstinspires.ftc.teamcode.ColorPipeline {
-        Mat inputMask = new Mat();
-        Size boxSize = new Size(5, 5);
-        Mat mask = new Mat(new Size(1282,722), org.opencv.core.CvType.CV_8U);
-        org.opencv.core.Point seed = new org.opencv.core.Point(640, 360);
-        Scalar one = new Scalar(1);
-
-
-
-        @Override
-        public Mat processFrame(@NonNull Mat input) {
-            //screen is 720 by 1280
-            input.copyTo(inputMask);
-//            Imgproc.Canny(input, inputMask, 250, 800);
-//            Imgproc.blur(inputMask, inputMask, boxSize);
-//            //assuming Mat.size() gets mat size
-//            Imgproc.floodFill(inputMask, mask, seed, one);
-//            Imgproc.threshold(inputMask,inputMask,254,255,Imgproc.THRESH_BINARY);
-//
-//            telemetry.addData("Image size: ", ""+input.size().toString());
-//            telemetry.addData("Mask size: ", ""+inputMask.size().toString());
-//            telemetry.update();
-
-//            input.mul(inputMask);
-
-            return input;
-        }
-
-        public char getColor() {
-            double[] rgb = inputMask.get(360, 640);
-            if (rgb != null) {
-                if (rgb.length == 4) {
-                    double r = rgb[0];
-                    double g = rgb[1];
-                    double b = rgb[2];
-                    return checkForColor(r, g, b, 70);
-                }
-            }
-            return 'a';
-        }
-
-        public double[] getMiddlePixel() {
-            return inputMask.get(640, 360);
-        }
-    }
-
     @Override
     public void runOpMode() {
 
