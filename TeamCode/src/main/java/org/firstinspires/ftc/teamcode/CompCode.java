b996095a25457ccda79a4d6163f9fa448ee9793a
FTC-6093 <6093deviation@gmail.com>
2022-11-18 16:15:03 -0600

    Servo Support
    QWERTYUIO

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CompCode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CompCode.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.util.ElapsedTime;
@@ -17,6 +18,8 @@ public class CompCode extends OpMode {
     private DcMotor FLDrive = null;
     private DcMotor BRDrive = null;
     private DcMotor VertLift = null;
+    private CRServo clawServo = null;
+    private CRServo clawUp = null;
 
 
     @Override
@@ -29,14 +32,16 @@ public class CompCode extends OpMode {
         FLDrive  = hardwareMap.get(DcMotor.class, "FLDrive");
         BRDrive  = hardwareMap.get(DcMotor.class, "BRDrive");
         VertLift  = hardwareMap.get(DcMotor.class, "VertLift");
+        clawServo = hardwareMap.get(CRServo.class, "ClawServo");
+        clawUp = hardwareMap.get(CRServo.class, "ClawUp");
 
         BLDrive.setDirection(DcMotor.Direction.FORWARD);
         BRDrive.setDirection(DcMotor.Direction.REVERSE);
         FRDrive.setDirection(DcMotor.Direction.REVERSE);
         FLDrive.setDirection(DcMotor.Direction.FORWARD);
         VertLift.setDirection(DcMotor.Direction.FORWARD);
-
-
+        clawServo.setDirection(CRServo.Direction.FORWARD);
+        clawUp.setDirection(CRServo.Direction.FORWARD);
 
 
         double pastMotorPower = 0;
@@ -60,11 +65,12 @@ public class CompCode extends OpMode {
         //Retrieve driving values from controller
         double y = gamepad1.left_stick_y * .8; // Is reversed
         double x = gamepad1.left_stick_x * .8;// Counteract imperfect strafing
-        double rx = -gamepad1.right_stick_x * .8;
-        boolean up;
-        boolean down;
-        up = (gamepad1.left_trigger > 0);
-        down = (gamepad1.right_trigger > 0);
+        double rx = gamepad1.right_stick_x * .8;
+        boolean up = (gamepad2.left_trigger > 0);
+        boolean down = (gamepad2.right_trigger > 0);
+        boolean coneGrab = (gamepad2.right_trigger > 0);
+        boolean coneDrop = (gamepad2.left_trigger > 0);
+        double coneRot = (gamepad2.left_stick_y * .8);
         // Denominator is the largest motor power (absolute value) or 1
         // This ensures all the powers maintain the same ratio, but only when at least one is out
         // of the range [-1, 1]
@@ -78,6 +84,22 @@ public class CompCode extends OpMode {
         FRDrive.setPower(frontRightPower);
         BLDrive.setPower(backLeftPower);
         BRDrive.setPower(backRightPower);
+
+        if(coneGrab && !coneDrop){
+            clawServo.setPower(.5);
+        } else if(coneDrop && !coneGrab){
+            clawServo.setPower(-.5);
+        } else {
+            clawServo.setPower(0);
+        }
+
+        if(coneRot > 0 && !(coneRot<1)){
+            clawUp.setPower(coneRot);
+        } else if(coneRot < 0 && !(coneRot>-1)){
+            clawUp.setPower(coneRot);
+        } else {
+            clawUp.setPower(0);
+        }
 
         if (down && !up) {
             VertLift.setPower(0.65);
